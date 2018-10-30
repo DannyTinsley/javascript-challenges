e30fc715fbe497db6bd3f4dfad6882f83d5df242
Danielle Tinsley <dtinsley000@nyc.yearup.org>
2018-10-30 15:59:32 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+for ( let i = 99; i >= 1; i--)
+{
+console.log (i +  "crows on the wall." + i + "crows.");
+console.log (1 + "fell down and became a wight.");
+console.log (i-1 + "crows on the wall");
+}
+
+if (i= 1);
+ {
+ console.log (i + "crow on the wall." + i + "single crow.");
+console.log ("It fell down and became a wight.");
+ console.log ("There's no one left to defend Westeros now.");
+ }
